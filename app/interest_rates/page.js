2ba920d84df350e358f68905db055064f2a115b9
erwannlenoach <erwannlenoach@hotmail.fr>
2024-06-18 17:19:25 +0200

    feat/adding-header-footer

---
+++ app/interest_rates/page.js
@@ -0,0 +1,13 @@
+"use client";
+import LoanForm from "../components/form/page";
+
+const interestRates = () => {
+ 
+  return (
+    <div className="uk-container">
+        <LoanForm/>
+    </div>
+  );
+};
+
+export default interestRates;
